4421acac60ba761f392e63dbb60455faec4aa712
Redelta <lucascampistron95@gmail.com>
2020-11-28 15:30:44 +0100

    build/feat(all): deploy the app via firebase / update README

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAr3312OuhvKcFofwZV7IMw4LuqUyzc3X',
+    authDomain: 'intern-test-abe11.firebaseapp.com',
+    databaseURL: 'https://intern-test-abe11.firebaseio.com',
+    projectId: 'intern-test-abe11',
+    storageBucket: 'intern-test-abe11.appspot.com',
+    messagingSenderId: '1059774781105',
+    appId: '1:1059774781105:web:1c238317f86ab3adc87490'
+  }
 };
